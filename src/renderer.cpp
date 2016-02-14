79edd471b01fde8fbfceaa12aad62558b26d0b3f
Alexander Korshun <alexander.korshun.96@gmail.com>
2016-02-14 14:49:31 +0300

    Oops. Removed debug from previous commit.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -34,8 +34,8 @@ void YE_Renderer()
     cam_x = player->X;
     cam_y = player->Y;
     glLoadIdentity();
-    glOrtho(0, 30, 0, 30, -1, 1);
-    //glTranslatef(-cam_x+half_width, -cam_y+half_height, 0.0f);
+    glOrtho(0, tiles_width, 0, tiles_height, -1, 1);
+    glTranslatef(-cam_x+half_width, -cam_y+half_height, 0.0f);
 
     glColor3f(stmap.Rcolor, stmap.Gcolor, stmap.Bcolor);
 
